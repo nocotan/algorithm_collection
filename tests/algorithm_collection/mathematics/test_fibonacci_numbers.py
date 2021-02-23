8417834d6ec851706fd75301072cd392ece19cbf
Masanari Kimura <noconoco.lib@gmail.com>
2021-02-24 02:22:29 +0900

    fibonacci numbers optimized

--- tests/algorithm_collection/mathematics/test_fibonacci_numbers.py
+++ tests/algorithm_collection/mathematics/test_fibonacci_numbers.py
@@ -3,6 +3,7 @@ import unittest
 from algorithm_collection.mathematics import fibonacci_numbers
 from algorithm_collection.mathematics import fibonacci_numbers_dp
 from algorithm_collection.mathematics import fibonacci_numbers_space_optimized
+from algorithm_collection.mathematics import fibonacci_numbers_optimized
 from algorithm_collection.mathematics import fibonacci_numbers_analytical
 
 
@@ -63,6 +64,25 @@ class TestFibonacciNumbers(unittest.TestCase):
         self.assertEqual(fibonacci_numbers_space_optimized(case_6["n"]), case_6["output"])
         self.assertEqual(fibonacci_numbers_space_optimized(case_7["n"]), case_7["output"])
         self.assertEqual(fibonacci_numbers_space_optimized(case_8["n"]), case_8["output"])
+
+    def test_fibonacci_numbers_optimized(self):
+        case_1 = {"n": 0, "output": 0}
+        case_2 = {"n": 1, "output": 1}
+        case_3 = {"n": 2, "output": 1}
+        case_4 = {"n": 3, "output": 2}
+        case_5 = {"n": 4, "output": 3}
+        case_6 = {"n": 5, "output": 5}
+        case_7 = {"n": 6, "output": 8}
+        case_8 = {"n": 7, "output": 13}
+
+        self.assertEqual(fibonacci_numbers_optimized(case_1["n"]), case_1["output"])
+        self.assertEqual(fibonacci_numbers_optimized(case_2["n"]), case_2["output"])
+        self.assertEqual(fibonacci_numbers_optimized(case_3["n"]), case_3["output"])
+        self.assertEqual(fibonacci_numbers_optimized(case_4["n"]), case_4["output"])
+        self.assertEqual(fibonacci_numbers_optimized(case_5["n"]), case_5["output"])
+        self.assertEqual(fibonacci_numbers_optimized(case_6["n"]), case_6["output"])
+        self.assertEqual(fibonacci_numbers_optimized(case_7["n"]), case_7["output"])
+        self.assertEqual(fibonacci_numbers_optimized(case_8["n"]), case_8["output"])
 
     def test_fibonacci_numbers_analytical(self):
         case_1 = {"n": 0, "output": 0}
