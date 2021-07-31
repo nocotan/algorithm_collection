b8c03247ab89d2ec9b9b2bc2b232679fcbf4f92e
Masanari Kimura <noconoco.lib@gmail.com>
2021-07-31 15:39:45 +0900

    result

--- langs/rust/basic_syntax/src/main.rs
+++ langs/rust/basic_syntax/src/main.rs
@@ -62,4 +62,9 @@ fn main() {
     if let Ok(code) = result {
         println!("code: {}", code);
     }
+
+    let result: Result<i32, String> = Ok(200);
+    println!("code: {}", result.unwrap_or(-1));
+    let result: Result<i32, String> = Err("error".to_string());
+    println!("code: {}", result.unwrap_or(-1));
 }
