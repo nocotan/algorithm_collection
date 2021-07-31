42ecc5a732160fb8467e0b2a9f457f0e3bc9ffd2
Masanari Kimura <noconoco.lib@gmail.com>
2021-07-31 15:34:27 +0900

    result

--- langs/rust/basic_syntax/src/main.rs
+++ langs/rust/basic_syntax/src/main.rs
@@ -50,4 +50,11 @@ fn main() {
     println!("{:?}", e1);
     println!("{:?}", e2);
     println!("{:?}", e3);
+
+    // result
+    let result: Result<i32, String> = Ok(200);
+    match result {
+        Ok(code) => println!("code: {}", code),
+        Err(err) => println!("Err: {}", err),
+    };
 }
