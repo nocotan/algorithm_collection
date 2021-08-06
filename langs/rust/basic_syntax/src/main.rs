d2f3253009e84fefcc349289cee57ebef11650cc
Masanari Kimura <noconoco.lib@gmail.com>
2021-08-07 08:53:57 +0900

    if

--- langs/rust/basic_syntax/src/main.rs
+++ langs/rust/basic_syntax/src/main.rs
@@ -116,4 +116,14 @@ fn main() {
     mut_val += immut_val;
     println!("{}", immut_val);
     println!("{}", mut_val);
+
+    // if
+    let number = 1;
+    if 0 < number {
+        println!("0 < number");
+    } else if number < 0 {
+        println!("number < 0");
+    } else {
+        println!("0 == number");
+    }
 }
